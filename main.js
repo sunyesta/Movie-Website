395228100495313d32c850225c384d6722f54e02
Mary Mac <marian.zlateva@gmail.com>
2023-04-15 17:40:30 +0200

    updated

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ app.set("json spaces", 2);
 app.locals.basedir = path.join(__dirname, "/pug");
 
 // external packages
-app.use("/group35", express.static("/data/home/group35/Movie-Website/public"));
+app.use(express.static("http://webtech.science.uu.nl/group35/"));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: false })); //access form variables inside post method
 app.use(flash());
 app.use(
